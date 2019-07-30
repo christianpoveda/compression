519eee38d0366094614b76f8d33738d2e33d82dd
Christian Poveda <cpovedar@fnal.gov>
2019-07-30 11:18:09 -0500

    Generate coding from tree

--- lib/huffman/src/lib.rs
+++ lib/huffman/src/lib.rs
@@ -8,12 +8,48 @@ pub struct HuffmanCoding {
 
 impl HuffmanCoding {
     pub fn from_freqs(freqs: &[usize]) -> Self {
+        let tree = Node::from_freqs(freqs);
+        let mut map = vec![BitVec::new(); freqs.len()];
+        Self::step(&mut map, &tree);
+        HuffmanCoding { map }
+    }
+
+    fn step(map: &mut [BitVec], tree: &Node) {
+        match &tree.content {
+            Internal(left, right) => {
+                left.push_bit(false, map);
+                right.push_bit(true, map);
+
+                Self::step(map, &left);
+                Self::step(map, &right);
+            }
+            Leaf(_) => {}
+        }
+    }
+
+    pub fn get(&self, pos: usize) -> Option<&BitVec> {
+        self.map.get(pos)
+    }
+}
+
+enum Content {
+    Leaf(usize),
+    Internal(Box<Node>, Box<Node>),
+}
+
+struct Node {
+    freq: isize,
+    content: Content,
+}
+
+impl Node {
+    fn from_freqs(freqs: &[usize]) -> Self {
         let mut queue = Vec::new();
 
         for (ind, &freq) in freqs.into_iter().enumerate() {
             queue.push(Node {
                 freq: -(freq as isize),
-                content: Leaf { ind },
+                content: Leaf(ind),
             });
         }
 
@@ -24,33 +60,26 @@ impl HuffmanCoding {
 
             let node = Node {
                 freq,
-                content: Internal { left, right },
+                content: Internal(left, right),
             };
 
             match queue.binary_search_by_key(&freq, |node| node.freq) {
-                Ok(i) | Err(i) => queue.insert(i, node),
+                Ok(i) | Err(i) => {
+                    queue.insert(i, node);
+                }
             };
-
         }
 
-        let tree = &queue[0];
+        queue.pop().unwrap()
+    }
 
-        HuffmanCoding {
-            map: (0..freqs.len()).map(|i| tree.vec(i)).collect(),
+    fn push_bit(&self, bit: bool, vecs: &mut [BitVec]) {
+        match &self.content {
+            Leaf(ind) => vecs[*ind].push(bit),
+            Internal(left, right) => {
+                left.push_bit(bit, vecs);
+                right.push_bit(bit, vecs);
+            }
         }
     }
-
-    pub fn get(&self, pos: usize) -> Option<&BitVec> {
-        self.map.get(pos)
-    }
-}
-
-struct Node {
-    freq: isize,
-    content: Content,
-}
-
-enum Content {
-    Leaf { ind: usize },
-    Internal { left: Box<Node>, right: Box<Node> },
 }
