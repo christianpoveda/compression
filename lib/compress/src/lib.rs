50c6d65f52ffe64beed414669ac9377274e425c5
Christian Poveda <cpovedar@fnal.gov>
2019-07-31 14:52:08 -0500

    Add compressor struct

--- lib/compress/src/lib.rs
+++ lib/compress/src/lib.rs
@@ -1,3 +1,32 @@
-pub trait Code<T, V> {
-    fn transform(&self, symbol: &T) -> Option<&V>;
+use std::marker::PhantomData;
+
+use bitvec::BitVec;
+
+pub trait Code<T, U> {
+    fn transform(&self, symbol: &T) -> Option<&U>;
+}
+
+pub struct Compressor<T, U> {
+    code: U,
+    _t: PhantomData<T>,
+}
+
+impl<T, U: Code<T, BitVec>> Compressor<T, U> {
+    pub fn from_code(code: U) -> Self {
+        Compressor {
+            code,
+            _t: PhantomData,
+        }
+    }
+
+    pub fn compress(&self, data: &[T]) -> Option<BitVec> {
+        let mut buffer = BitVec::new();
+
+        for symbol in data {
+            let result = self.code.transform(symbol)?.clone();
+            buffer.concat(result);
+        }
+
+        Some(buffer)
+    }
 }
