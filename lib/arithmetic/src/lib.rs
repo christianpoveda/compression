f09fe8c7bc7bc681106b0439a7aef9f8b9377a5b
Christian Poveda <christianpoveda@protonmail.com>
2019-09-03 12:41:28 -0500

    Update arithmetic encoding

--- lib/arithmetic/src/lib.rs
+++ lib/arithmetic/src/lib.rs
@@ -1,58 +1,15 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
 use std::hash::Hash;
+
+use bitvec::BitVec;
 
 pub struct ArithmeticCode<T> {
     map: HashMap<T, Interval>,
+    total: u32,
 }
-impl<T> ArithmeticCode<T>
-where
-    T: Clone + Hash + Eq + std::fmt::Debug,
-{
-    pub fn recover(&self, data: u64) -> Option<Vec<T>> {
-        println!("looking for: {:?}", data);
 
-        let mut base = Interval::new(0, std::u64::MAX);
-        let mut buffer = Vec::new();
-        let mut go = true;
-        while go {
-            for (symbol, interval) in &self.map {
-                let interval: Interval = base.contained(*interval)?;
-                go = interval.len() > 1;
-                println!("checking {:?}", interval);
-                if interval.contains(data) {
-                    buffer.push(symbol.clone());
-                    base = interval;
-                    println!("found");
-                    break;
-                }
-            }
-        }
-        Some(buffer)
-    }
-
-    pub fn transform(&self, data: &[T]) -> Option<u64> {
-        let mut data_iter = data.iter();
-        let mut interval = *self.map.get(data_iter.next()?)?;
-        for symbol in data_iter {
-            interval = interval.contained(*self.map.get(symbol)?)?;
-        }
-        Some(interval.top)
-    }
-
-    pub fn from_data(data: &[T]) -> Self {
-        let counts = Self::count_symbols(data);
-        let total = data.len() as u64;
-
-        let mut map = HashMap::new();
-        let mut acc = 0;
-
-        for (symbol, count) in counts {
-            let length = ((std::u64::MAX as u128 * count as u128) / total as u128) as u64;
-            map.insert(symbol, Interval::new(acc, acc + length));
-            acc += length;
-        }
-        ArithmeticCode { map }
-    }
+impl<T: Debug + Clone + Eq + Hash> ArithmeticCode<T> {
     fn count_symbols(data: &[T]) -> Vec<(T, usize)> {
         let mut counts: HashMap<T, usize> = HashMap::new();
 
@@ -69,32 +26,69 @@ where
         counts.sort_by_key(|x| -(x.1 as isize));
         counts
     }
+
+    pub fn from_data(data: &[T]) -> Self {
+        let mut map = HashMap::new();
+        let counts = Self::count_symbols(data);
+        let mut low_count = 0;
+        for (symbol, count) in counts {
+            let high_count = low_count + count as u32;
+            map.insert(symbol, Interval::new(low_count, high_count));
+            low_count = high_count;
+        }
+
+        ArithmeticCode {
+            map,
+            total: low_count,
+        }
+    }
+
+    pub fn transform(&self, data: &[T]) -> Option<u32> {
+        let mut int = Interval::new(0, std::u32::MAX.wrapping_shr(1));
+
+        for symbol in data {
+            let Interval {
+                low: low_count,
+                high: high_count,
+            } = self.map.get(symbol)?;
+            if int.high <= int.low {
+                return None;
+            }
+            let step = (int.high - int.low + 1) / self.total;
+            int.high = int.low + step * high_count - 1;
+            int.low += step * low_count;
+        }
+
+        Some((int.high + int.low) / 2)
+    }
+
+    pub fn recover(&self, buffer: u32, len: usize) -> Option<Vec<T>> {
+        let mut result = Vec::new();
+        let mut int = Interval::new(0, std::u32::MAX.wrapping_shr(1));
+        for _ in 0..len {
+            let step = (int.high - int.low - 1) / self.total;
+            let value = (buffer - int.low) / step;
+            for (symbol, count) in &self.map {
+                if value < count.high && value >= count.low {
+                    result.push(symbol.clone());
+                    int.high = int.low + step * count.high - 1;
+                    int.low += step * count.low;
+                    break;
+                }
+            }
+        }
+        Some(result)
+    }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 struct Interval {
-    bot: u64,
-    top: u64,
+    low: u32,
+    high: u32,
 }
 
 impl Interval {
-    fn new(bot: u64, top: u64) -> Self {
-        Interval { bot, top }
-    }
-
-    fn len(&self) -> u64 {
-        self.top - self.bot
-    }
-
-    fn contained(self, other: Self) -> Option<Self> {
-        let len = self.len();
-        Some(Self::new(
-            self.bot + ((other.bot as u128 * self.len() as u128) / std::u64::MAX as u128) as u64,
-            self.bot + ((other.top as u128 * self.len() as u128) / std::u64::MAX as u128) as u64,
-        ))
-    }
-
-    fn contains(&self, value: u64) -> bool {
-        self.top > value && self.bot <= value
+    fn new(low: u32, high: u32) -> Self {
+        Interval { low, high }
     }
 }
--- lib/arithmetic/tests/lib.rs
+++ lib/arithmetic/tests/lib.rs
@@ -5,9 +5,12 @@ use arithmetic::ArithmeticCode;
 #[test]
 fn foo() {
     let data = b"A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
-    // let data = b"AAAAAAAAAAAAAAABBBBBBBCCCCCCDDDDDDEEEEE";
+    let msg = b"A_DEAD_DAD_";
     let code = ArithmeticCode::from_data(data);
-    let comp = code.transform(data);
-    println!("{:?} -> {:?}", (String::from_utf8(data.to_vec())), comp);
-    println!("{:?}", String::from_utf8(code.recover(comp.unwrap()).unwrap()));
+    let comp = code.transform(msg).unwrap();
+    println!("{:?} -> {:0x}", (String::from_utf8(msg.to_vec())), comp);
+    println!(
+        "{:?}",
+        String::from_utf8(code.recover(comp, msg.len()).unwrap())
+    );
 }
